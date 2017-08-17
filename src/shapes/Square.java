afcd10f1cf759d1b9516231d17ec548b9f95cef9
Rupalí <rupali.lopez@hotmail.com>
2017-08-16 22:17:09 -0300

    tarea0 modificada

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,38 +2,10 @@ package shapes;
 
 import geoTools.CartesianPoint;
 
-public class Square extends AbstractShape {
-	private double side;
+public class Square extends Rectangle {
 
 	public Square(CartesianPoint c, double a) {
-		super(c);
-		side = a;
-	}
-
-	@Override
-	public double getArea() {
-		return side * side;
-	}
-
-	@Override
-	public double getPerimeter() {
-		return 4 * side;
-	}
-
-	@Override
-	public double getWidth() {
-		return side;
-	}
-
-	@Override
-	public double getHeight() {
-		return side;
-	}
-
-	@Override
-	public String toString() {
-		return "Square centered in (" + this.getPosition().getX() + ","
-				+ this.getPosition().getY() + ")";
+		super(c, a, a);
 	}
 
 }
